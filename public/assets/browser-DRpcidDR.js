744f868fde7b0ae43a8abd2e7ac82024db6c46e0
Swelo-ui <imgamer.ms@gmail.com>
2025-05-17 17:34:59 +0530

    fix: Install terser and complete local build

---
+++ public/assets/browser-DRpcidDR.js
@@ -0,0 +1,2 @@
+import{k as e}from"./vendor-COH2D0_o.js";const o=e((function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")})),t=Object.freeze(Object.defineProperty({__proto__:null,default:o},Symbol.toStringTag,{value:"Module"}));export{t as b};
+//# sourceMappingURL=browser-DRpcidDR.js.map
